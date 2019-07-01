cbbbd5830e37ac865d94a217e23dfcbc2d24f90b
Yaz Khoury <yaz.khoury@gmail.com>
2019-06-30 21:45:10 -0400

    feat: Add elliptic curve point logic and tests

--- ecc.py
+++ ecc.py
@@ -42,7 +42,7 @@ class FieldElement(object):
         return self.__class__(num, self.prime)
 
     def __pow__(self, exponent):
-        n = exponent % (self.prime - 1)  # <1>
+        n = exponent % (self.prime - 1)
         num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
 
@@ -55,3 +55,52 @@ class FieldElement(object):
         # 1/n == pow(n, p-2, p)
         num = self.num * pow(other.num, self.prime-2, self.prime) % self.prime
         return self.__class__(num, self.prime)
+
+
+class Point(object):
+
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        return not(self == other)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        else:
+            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {}, {} are not on the same curve'.format
+            (self, other))
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+        if (self.x == other.x) and (self.y != other.y):
+            return self.__class__(None, None, self.a, self.b)
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x = s**2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
--- tests/test_ecc.py
+++ tests/test_ecc.py
@@ -47,3 +47,28 @@ class FieldElementTest(TestCase):
         a = FieldElement(4, 31)
         b = FieldElement(11, 31)
         self.assertEqual(a**-4 * b, FieldElement(13, 31))
+
+
+class PointTest(TestCase):
+    def test_ne(self):
+        a = Point(x=3, y=-7, a=5, b=7)
+        b = Point(x=18, y=77, a=5, b=7)
+        self.assertTrue(a != b)
+        self.assertFalse(a != a)
+
+    def test_add0(self):
+        a = Point(x=None, y=None, a=5, b=7)
+        b = Point(x=2, y=5, a=5, b=7)
+        c = Point(x=2, y=-5, a=5, b=7)
+        self.assertEqual(a + b, b)
+        self.assertEqual(b + a, b)
+        self.assertEqual(b + c, a)
+
+    def test_add1(self):
+        a = Point(x=3, y=7, a=5, b=7)
+        b = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a + b, Point(x=2, y=-5, a=5, b=7))
+
+    def test_add2(self):
+        a = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a + a, Point(x=18, y=77, a=5, b=7))
